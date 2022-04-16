1c391a48fa895b301a2af2440609cf8a64192fef
renato.cunha <renato.cunha@totvs.com.br>
2022-04-16 20:50:56 -0300

    Implements login with google button

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,16 +1,73 @@
-import React from 'react'
-import BaseComponent from "./BaseComponent/BaseComponent";
+import React, { Component } from 'react';
+import BaseComponent from './BaseComponent/BaseComponent';
+import { GoogleLogin, GoogleLogout } from 'react-google-login';
 
-const Login = () => {
-    return (
-        <>
-            <BaseComponent >
-                <div className='container'>
-                    "Autenticate with google"
-                </div>
-            </BaseComponent>
-        </>
-  )
+class Login extends Component {
+	
+	constructor() {
+		super();
+		this.appId = '963927842810-or0j5j8kmvf4c01hd15bbompaqut7qpu.apps.googleusercontent.com';
+		this.state = {
+			isLoggedIn: false,
+			userGoogleData: {
+				id: '',
+				name: '',
+				email: '',
+				photo: '',
+			},
+		};
+	}
+
+	loginSuccess = (response) => {
+		let userGoogleData = {
+			id: response.profileObj.googleId,
+			name: response.profileObj.name,
+			email: response.profileObj.email,
+			photo: response.profileObj.imageUrl,
+		};
+		this.setState({ userGoogleData, isLoggedIn: true });
+	};
+
+	loginError = (response) => {
+		console.error(response);
+	};
+
+	logout = (response) => {
+		console.log(response);
+		let userGoogleData = {
+			id: '',
+			name: '',
+			email: '',
+			photo: '',
+		};
+		this.setState({ userGoogleData, isLoggedIn: false });
+	};
+
+	render() {
+		return (
+			<BaseComponent>
+				<div className="container">
+					{this.state.isLoggedIn ? (
+						<div>
+							<GoogleLogout
+								clientId={this.appId}
+								buttonText={'Sair'}
+								onLogoutSuccess={this.logout}
+							></GoogleLogout>
+						</div>
+					) : (
+						<GoogleLogin
+							clientId={this.appId}
+							buttonText="Login usando Google"
+							onSuccess={this.loginSuccess}
+							onFailure={this.loginError}
+							isSignedIn={true}
+						/>
+					)}
+				</div>
+			</BaseComponent>
+		);
+	}
 }
 
-export default Login
+export default Login;
