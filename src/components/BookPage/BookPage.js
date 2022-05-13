78abaafadae95dad33d30a83a28bd3d8f2669ed0
Giovana <giovanasachett@gmail.com>
2022-05-13 19:04:31 -0300

    refactor bookpage

--- src/components/BookPage/BookPage.js
+++ src/components/BookPage/BookPage.js
@@ -1,45 +1,51 @@
-import React, { useState} from 'react'
+import React, { useEffect, useState } from 'react'
 import {
     useParams
-  } from "react-router-dom";
+} from "react-router-dom";
 import { Rating, CircularProgress } from '@mui/material'
 import cookie from 'react-cookies';
 import './BookPage.css'
 import '../../App.css'
 
+const initialState = {
+    loading: true,
+    data: null,
+    error: false
+};
 
-let bookInfo = undefined;
 
 function BookPage() {
-    const [loading, setLoading] = useState(true)
+    const [{ bookInfo, loading, error }, setState] = useState(initialState)
+
 
     let { short } = useParams();
 
-    async function featchBookInfo () {
+    useEffect(() => {
         console.log('fetching books');
         const url = `https://cherry-tart-55973.herokuapp.com/api/v1/b/${short}`;
         // const url = `http://localhost:8077/api/v1/b/${id}`
-        const response = await fetch(url, {
+        fetch(url, {
             method: 'GET',
             headers: {
                 'Authorization': cookie.load('loginToken'),
             }
-        });
+        }).then(response => 
+            response.json().then(data => ({
+                data: data,
+                status: response.status
+            })
+        ).then(res => {
+            setState({ loading: false, bookInfo: res.data, error: false })
+            // console.log(res.status, res.data)
+        }));
 
-        const bookResponse = await response.json();
-        bookInfo = bookResponse
+    }, [short])
 
-        setLoading(false);
-    }
-
-	
-    console.log('rendering');
     if (loading) {
-        featchBookInfo()
         return (
             <div className='progressbar'>
-				<CircularProgress />
-			</div>
+                <CircularProgress />
+            </div>
         );
     }
 
@@ -47,20 +53,19 @@ function BookPage() {
         return <div>Unable to find book</div>;
     }
 
-    if(bookInfo.error) {
+    if (error) {
         return <div>{bookInfo.error}</div>
     }
 
-    console.log('book page with info')
     return (
         <div className='bookpage'>
-            <img src={bookInfo.imageLinks.mainImage} className='bookcover'/>
+            <img src={bookInfo.imageLinks.mainImage} className='bookcover' />
             <h1>{bookInfo.title}</h1>
             <h2>{bookInfo.authors[0]}</h2>
             <Rating name="no-value" value={null} />
         </div>
     );
-	
+
 }
 
 export default BookPage;
